c741bbbea7d5d23e4d5f6a3f516e05bf11572ecb
Luke Sheppard <luke.sheppard@queensu.ca>
2022-08-01 13:04:15 -0400

    post reordering based on number of likes, active comments and newest, added to the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,21 @@ import { db } from "../firebase-config";
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
+  const [order, setOrder] = useState("new");
 
   useEffect(() => {
+    const getQueryType = (recipesRef) => {
+      if (order === "new") {
+        return query(recipesRef, orderBy("createdAt", "desc"));
+      } else if (order === "top") {
+        return query(recipesRef, orderBy("likesCount", "desc"));
+      } else {
+        return query(recipesRef, orderBy("commentCount", "desc"));
+      }
+    };
+
     const recipesRef = collection(db, "recipes");
-    const recipesQuery = query(recipesRef, orderBy("createdAt", "desc"));
+    const recipesQuery = getQueryType(recipesRef);
     onSnapshot(recipesQuery, (snapshot) => {
       const recipes = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -17,7 +28,7 @@ const Home = () => {
       }));
       setRecipes(recipes);
     });
-  }, []);
+  }, [order]);
 
   return (
     <Grid container spacing={3} sx={{ mt: 12 }}>
@@ -27,17 +38,29 @@ const Home = () => {
           Order Recipes By:
         </Typography>
         <Container>
-          <Button variant="contained" sx={{ ml: 3 }}>
+          <Button
+            variant="contained"
+            sx={{ ml: 3 }}
+            onClick={() => setOrder("new")}
+          >
             NEWEST
           </Button>
         </Container>
         <Container>
-          <Button variant="contained" sx={{ ml: 3, mt: 4 }}>
+          <Button
+            variant="contained"
+            sx={{ ml: 3, mt: 4 }}
+            onClick={() => setOrder("top")}
+          >
             TOP RATED
           </Button>
         </Container>
         <Container sx={{ mr: 5 }}>
-          <Button variant="contained" sx={{ ml: 3, mt: 4 }}>
+          <Button
+            variant="contained"
+            sx={{ ml: 3, mt: 4 }}
+            onClick={() => setOrder("active")}
+          >
             MOST ACTIVE
           </Button>
         </Container>
