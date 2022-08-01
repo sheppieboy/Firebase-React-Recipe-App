b28a6a18a747b332da804396482e16342f08f759
Luke Sheppard <luke.sheppard@queensu.ca>
2022-08-01 03:37:46 -0400

    Like Recipe functionality added.  This included updating the likes array in the db and the display of the like button indicating blue for whether a post has been linked and dull grey for when a post has not been liked.

--- src/components/LikeRecipe.jsx
+++ src/components/LikeRecipe.jsx
@@ -1,28 +1,37 @@
 import { ThumbUp } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase-config";
 
-const LikeRecipe = ({ id, likes }) => {
+const LikeRecipe = ({ id, likes, likesCount }) => {
   const [user] = useAuthState(auth);
+
   const likeRef = doc(db, "recipes", id);
   const handleLike = () => {
     //remove the like from the likes array in the db
     if (likes?.includes(user.uid)) {
-      updateDoc(likeRef, { likes: arrayRemove(user.id) }).catch((err) => {
+      updateDoc(likeRef, {
+        likes: arrayRemove(user.uid),
+      }).catch((err) => {
         console.error(err);
       });
     } //add a like to the likes array in the db
     else {
-      updateDoc(likeRef, { likes: arrayUnion(user.uid) }).catch((err) => {
+      updateDoc(likeRef, {
+        likes: arrayUnion(user.uid),
+      }).catch((err) => {
         console.error(err);
       });
     }
   };
   return (
     <IconButton>
-      <ThumbUp color="primary" onClick={handleLike} />
+      <ThumbUp
+        sx={{ color: likes?.includes(user.uid) ? "blue" : null }}
+        onClick={handleLike}
+      />
     </IconButton>
   );
 };
